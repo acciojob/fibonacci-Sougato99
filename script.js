a4f3262d740103f94405f9778e1c2854893f8c0d
Sougato99 <128289022+Sougato99@users.noreply.github.com>
2024-08-26 22:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	if(num == 1) return 0;
+	var a = 0, b = 1;
+	for(var c = 0; c < num; c++){
+		var res = a + b;
+		a = b;
+		b = res;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
