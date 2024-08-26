1f4886284add7e72b1162aef49ee120195826199
Sougato99 <128289022+Sougato99@users.noreply.github.com>
2024-08-26 22:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fibonacci(num) {
 // your code here
 	if(num == 1) return 0;
 	var a = 0, b = 1;
-	for(var c = 0; c < num; c++){
+	for(var c = 1; c < num; c++){
 		var res = a + b;
 		a = b;
 		b = res;
